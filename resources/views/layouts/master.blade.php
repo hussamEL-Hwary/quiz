0815b76b971a0e94c377e017cbed147541b8cc86
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2017-08-14 13:36:04 +0200

    add master view

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+      <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+   <body>
+     @yield('content')
+   </body>
+
+  <script src="/js/login.js"></script>
+</body>
+
+</html>
