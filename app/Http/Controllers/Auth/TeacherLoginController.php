b296412ad838bbbfc50820db59bcffbf5fdac155
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2017-08-17 01:59:52 +0200

    add teacher login controller

---
+++ app/Http/Controllers/Auth/TeacherLoginController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use auth;
+class TeacherLoginController extends Controller
+{
+
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest:teacher')->except('logout');
+    }
+
+    /*
+    *this function returns teacher login form
+    */
+    public function showLoginForm()
+    {
+      return view('auth.teacherlogin');
+    }
+
+    /*
+    *this function logs teacher in
+    */
+    public function login(Request $request)
+    {
+      //validate form data
+      $this->validate($request,[
+        'email'=>'required|email',
+        'password'=>'required'
+      ]);
+      //try to login
+      if(Auth::guard('teacher')->attempt(['email'=>$request->email,'password'=>$request->password],$request->remember))
+      {
+         //if success redirect to teacher homepage
+         return redirect()->intended(route('teacher.dashboard'));
+      }
+      //if not sucess redirect back with form data excep password
+      return redirect()->back()->withInput($request->only('email','remember'));
+    }
+}
