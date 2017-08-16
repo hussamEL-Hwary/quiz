7291db22da74a0a0deddaec62318152611654a71
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2017-08-16 18:06:18 +0200

    add nav bar

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,27 @@
+<div class="main-navigation navbar-fixed-top">
+		<nav class="navbar navbar-default">
+			<div class="container">
+			<div class="navbar-header">
+			  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+			    <span class="icon-bar"></span>
+			    <span class="icon-bar"></span>
+			    <span class="icon-bar"></span>
+			  </button>
+			  <a class="navbar-brand" href="#">اسم الموقع </a>
+			</div>
+			<div class="collapse navbar-collapse" id="myNavbar">
+			  <ul class="nav navbar-nav navbar-right">
+			    <li class="active"><a href="#">1</a></li>
+			    <li><a href="signup.html">2</a></li>
+			    <li><a href="#">3</a></li>
+			    <li><a href="#">4</a></li>
+			    <li><a href="#">5</a></li>
+			  </ul>
+			</div>
+		  </div>
+		</nav>
+	</div>
+  <body>
+  <div><br/>
+  <br/><br/><br/><br/><br/>
+  </div>
