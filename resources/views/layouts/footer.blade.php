d9ee6b66604869965aa334c875a83b38607aaf70
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2017-08-16 18:04:14 +0200

    add footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,58 @@
+
+	<!--FOOTER START-->
+	<footer>
+	<footer class="footer section-padding">
+		<div class="container">
+			<div class="row">
+				<div style="visibility: visible; animation-name: zoomIn;" class="col-sm-12 text-center wow zoomIn">
+					<h3>Follow us on</h3>
+					<div class="footer_social">
+						<ul>
+							<li><a class="f_facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a class="f_twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+							<li><a class="f_google" href="#"><i class="fa fa-google-plus"></i></a></li>
+							<li><a class="f_linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+							<li><a class="f_instagram" href="#"><i class="fa fa-instagram"></i></a></li>
+
+						</ul>
+					</div>
+				</div><!--- END COL -->
+			</div><!--- END ROW -->
+
+
+		</div><!--- END CONTAINER -->
+<div class="container2">
+			<div class="row">
+				<div style="visibility: visible; animation-name: zoomIn;" class="col-sm-12 text-center wow zoomIn">
+					<h3>About Us </h3>
+					<p> helovdfkvmfdkljvmdflk <br/>helovdfkvmfdkljvmdflk <br/></p>
+				</div><!--- END COL -->
+			</div><!--- END ROW -->
+
+
+		</div><!--- END CONTAINER -->
+		<div class="container3">
+			<div class="row">
+				<div style="visibility: visible; animation-name: zoomIn;" class="col-sm-12 text-center wow zoomIn">
+						<div class="links">
+						<ul>
+							<li><a  href="contact.html"> Contact Us </a></li>
+							<li><a  href="#"> Terms of services</i></a></li>
+							<li><a  href="#"> Privacy policy </i></a></li>
+							<li><a  href="#"> News</i></a></li>
+
+						</ul>
+					</div>
+				</div><!--- END COL -->
+			</div><!--- END ROW -->
+	</footer>
+	<!--FOOTER END-->
+	<div class="footer-bottom">
+		<div class="container">
+			<div style="visibility: visible; animation-name: zoomIn;" class="col-md-12 text-center wow zoomIn">
+				<div class="footer_copyright">
+					<p> © 2017 Copyright, All Rights Reserved.</p>
+				</div>
+			</div>
+		</div>
+	</div>
