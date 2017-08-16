2231cfa93597e2affa02880ca184a017fe26fcb3
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2017-08-16 20:42:10 +0200

    add fillable items

--- app/Student.php
+++ app/Student.php
@@ -3,8 +3,28 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class student extends Model
+class student extends Authenticatable
 {
-    //
+  use Notifiable;
+  protected $gured="student";
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'first_name','last_name', 'email', 'password','birthday','activated',
+  ];
+
+  /**
+   * The attributes that should be hidden for arrays.
+   *
+   * @var array
+   */
+  protected $hidden = [
+      'password', 'remember_token',
+  ];
 }
--- app/Teacher.php
+++ app/Teacher.php
@@ -3,8 +3,29 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class teacher extends Model
+class teacher extends Authenticatable
 {
-    //
+    use Notifiable;
+    protected $guard="teacher";
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'first_name','last_name', 'email', 'password','birthday','activated',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 }
