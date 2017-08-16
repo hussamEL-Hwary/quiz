c41a3036d6e611fb1ba298c3ab68d6caad80b9a8
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2017-08-16 18:07:05 +0200

    add css js files

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,16 +1,32 @@
 <!DOCTYPE html>
-<html >
+<html lang="en" class=" js no-touch">
 <head>
-  <meta charset="UTF-8">
-  <title></title>
-      <link rel="stylesheet" href="/css/style.css">
+ <title></title>
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <link rel="stylesheet" href="/css/bootstrap.min.css">
+ <link rel="stylesheet" type="text/css" href="/css/font-awesome.min.css">
+ <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,600|Raleway:600,300|Josefin+Slab:400,700,600italic,600,400italic' rel='stylesheet' type='text/css'>
+ <link rel="stylesheet" type="text/css" href="/css/style.css">
+ <link rel="stylesheet" type="text/css" href="/css/slick-team-slider.css"/>
+
 </head>
 <body>
-   <body>
-     @yield('content')
-   </body>
 
-  <script src="/js/login.js"></script>
+     @yield('content')
+
+
+
+  @include('layouts.footer')
+
+  <script src="/js/jquery.min.js"></script>
+  	<script src="/js/jquery.easing.min.js"></script>
+  	<script src="/js/bootstrap.min.js"></script>
+  	<script src="/js/jquery.mixitup.js" type="text/javascript"></script>
+	<script type="text/javascript" src="/js/slick.min.js"></script>
+	<script type="text/javascript" src="/js/custom.js"></script>
+    <script src="/contactform/contactform.js"></script>
+
 </body>
 
 </html>
