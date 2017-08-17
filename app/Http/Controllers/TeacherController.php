5cac87910dc26ecca43eabebecb830f49cbb7b6e
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2017-08-17 02:01:03 +0200

    index function

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Auth;
 class TeacherController extends Controller
 {
-    //
+
+    public function __construct()
+    {
+        $this->middleware('auth:teacher');
+    }
+
+    public function index()
+    {
+      return view('pages.teacher.home');
+    }
 }
