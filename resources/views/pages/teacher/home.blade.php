6c60db7576ff76dea5f6bd0598c4c72728c10936
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2017-08-19 06:30:38 +0200

    add teacher name

--- resources/views/pages/teacher/home.blade.php
+++ resources/views/pages/teacher/home.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.master')
 
 @section('content')
-<h1>Teacher view</h1>   
+
+<h2>Hi, {{Auth::user()->first_name}}</h2>
+<br>
+<h2>you are logged in as Teacher</h2>
+
+<br>
+<br><br><br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br>
 @endsection
