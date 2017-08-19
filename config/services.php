76a62f159654e01579d47ac9079e4711287d3cf8
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2017-08-19 06:24:22 +0200

    add socil log in providers id and secret id

--- config/services.php
+++ config/services.php
@@ -35,4 +35,33 @@ return [
         'secret' => env('STRIPE_SECRET'),
     ],
 
+    'github' => [
+    'client_id' => 'dde5547077afcaf34ad5',
+    'client_secret' => '043d6da4faeef879e7864fc8b7106c184268ee04',
+    'redirect' => 'http://localhost:8000/login/github/check',
+    ],
+
+    'facebook' => [
+    'client_id' => '113079409320606',
+    'client_secret' => '8bfafdf553de96ca536070d1523a3742',
+    'redirect' => 'http://localhost:8000/login/facebook/check',
+    ],
+
+    'instagram' => [
+    'client_id' => '94697aee94c342b3a4d3382e8373364f',
+    'client_secret' => '1385dd60711f43fdb789c8260ab5f1c3',
+    'redirect' => 'http://localhost:8000/login/instagram/check',
+    ],
+
+    'google' => [
+    'client_id' => '314240770116-e9mu89eej4easg136m2b9q1nmueiqv21.apps.googleusercontent.com',
+    'client_secret' => '1RoYGoD-plsIu7bLVwmYrEbB',
+    'redirect' => 'http://localhost:8000/login/google/check',
+    ],
+
+    'twitter' => [
+    'client_id' => 'your-github-app-id',
+    'client_secret' => 'your-github-app-secret',
+    'redirect' => 'http://localhost:8000/login/instagram/check',
+    ],
 ];
