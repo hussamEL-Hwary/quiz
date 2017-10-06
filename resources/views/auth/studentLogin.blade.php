9052f60fedda6bc80b7d7d4bc398f056ec6c88e6
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2017-10-06 22:04:52 +0200

    modefiy teacher login and student login page

--- resources/views/auth/studentLogin.blade.php
+++ resources/views/auth/studentLogin.blade.php
@@ -1,50 +1,54 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="container0">
-	<section id="content">
-		<form method="post" action="{{route('student.login.submit')}}">
+
+<div class="container">
+
+		<form method="post" class="form-group login"  action="{{route('student.login.submit')}}">
 			{{ csrf_field() }}
-			<h1>Student Login</h1>
-			<div>
-				<input type="text" name="email" placeholder="Email" required="" id="email" />
-			</div>
-			<div>
-				<input type="password" placeholder="Password" name="password" required="" id="password" />
-			</div>
-			<div>
-				<input type="submit" value="Log in" /> <br/>
+			<h3 class="text-center" >Student Login</h3>
 
+				<input type="email" class="form-control" placeholder="Email" required="" id="email" type="email" name="email" value="{{ old('email') }}"/>
+				@if ($errors->has('email'))
+        <span class="has-error">
+          <strong>{{ $errors->first('email') }}</strong>
+        </span>
+    		@endif
+
+				<input type="password" class="form-control"  placeholder="Password" required="" id="password" type="password" name="password" />
+					@if ($errors->has('password'))
+        		<span class="has-error">
+        		<strong>{{ $errors->first('password') }}</strong>
+        		</span>
+      		@endif
+
+				<input type="submit" class="btn btn-primary btn-block" value="Log in" />
+				<div class="checkbox">
 				<label for="rememberme">
-                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me.
+                <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}/> Remember me.
         </label>
-
+					</div>
 
 				<a href="{{ route('student.password.request') }}">Lost your password?</a>
+				<label for="">or</label>
 				<a href="{{ route('register') }}">Register</a>
 
-			</div>
+
 		</form><!-- form -->
 
-		<div class="button">
-			<h6>Or sign up using the following services :</h6><br/> <br/>
-			<a class="twitter" href="#" onclick="myFunction()"></a>
-            <a class="facebook" href="{{route('social.login',['provider' => 'facebook','type'=>'student'])}}" ></a>
-            <a class="google" href="{{route('social.login',['provider' => 'google','type'=>'student'])}}" ></a>
-            <a class="github" href="{{route('social.login',['provider' => 'github','type'=>'student'])}}" ></a>
-            <a class="instagram" href="#" ></a>
-		</div><!-- button -->
+			<hr>
+			<h5 class='text-center'>Or sign up using the following services :</h5><br/>
+			<div class="icons text-center">
 
-	</section><!-- content -->
+            <a  class="btn btn-primary x" href="{{route('social.login',['provider' => 'facebook','type'=>'student'])}}"><i class='fa fa-facebook fa-lg'></i></a>
+            <a class="btn btn-primary"    href="{{route('social.login',['provider' => 'google','type'=>'student'])}}" ><i class='fa fa-google-plus fa-lg'></i></a>
+            <a class="btn btn-primary z"  href="{{route('social.login',['provider' => 'github','type'=>'student'])}}"><i class='fa fa-github fa-lg'></i></a>
+
+			</div>
 </div><!-- container -->
 
-<div>
-	<br/>
-	<br/>
-  <br/>
-	<br/>
-  <br/>
-	<br/>
-</div>
+
+
+
 
 @endsection
