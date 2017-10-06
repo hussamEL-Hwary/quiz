976557326d1284d3cee53d963901e732fca623bd
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2017-10-06 22:14:26 +0200

    create category and first step of quiz page

--- resources/views/pages/teacher/createcategory.blade.php
+++ resources/views/pages/teacher/createcategory.blade.php
@@ -2,20 +2,27 @@
 
 @section('content')
 
-<form  action="{{route('category.submit')}}" method="post">
+
+<form class='create-cat'  action="{{route('category.submit')}}" method="post">
+  <h2 class='text-center'>Create New Category</h2>
+
 {{ csrf_field() }}
-<label>Category name </label>
-<input type="text" id="categoryname" name="categoryname"  required>
-<br>
-<label>description (optional)</label>
-<br>
-<textarea id="description" name="description" rows="8" cols="80" placeholder="write description to your category"></textarea>
-<br>
-<input type="submit" id="createcategory" name="create" value="Submit">
+<input type="text" id="categoryname" class='form-control' name="categoryname" placeholder='Category Name'  required>
+    @if($errors->has('categoryname'))
+        <span class="has-error">
+        <strong>{{ $errors->first('categoryname') }}</strong>
+        </span>
+    @endif
+<textarea id="description" class='form-control' name="description" rows="8" cols="80" placeholder="write description to your category"></textarea>
+@if($errors->has('description'))
+    <span class="has-error">
+    <strong>{{ $errors->first('description') }}</strong>
+    </span>
+@endif
+
+<input class='btn btn-primary btn-block' type="submit" id="createcategory" name="Create" value="Submit">
 
 </form>
 
-<br><br><br><br><br><br><br><br><br><br><br><br>
-<br><br><br><br><br><br><br><br><br><br><br><br>
-<br><br><br><br><br>
+
 @endsection
---
+++ resources/views/pages/teacher/createquiz.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.master')
+
+@section('content')
+<form class="create-quiz" action="/teacher/quiz" method="post">
+  <h2>First Step</h2>
+
+  {{csrf_field()}}
+  <input class='form-control' type="text" name="name" placeholder="Quiz Name" id="name" required><br>
+  @if($errors->has('name'))
+  <span class="">
+    <strong class='btn btn-danger btn-block'>{{ $errors->first('name') }}</strong>
+  </span>
+  @endif
+  <input class='form-control' type="number" name="count"  placeholder="Number of questions between 1 and 20" required><br>
+  @if($errors->has('count'))
+  <span class="">
+    <strong class='btn btn-danger btn-block'>{{ $errors->first('count') }}</strong>
+  </span>
+  @endif
+  <select class="form-control" name="categoryName">
+    <option value="math">Math</option>
+    <option value="codeing">codeing</option>
+  </select>
+  @if($errors->has('categoryName'))
+  <span class="">
+    <strong class='btn btn-danger btn-block'>{{ $errors->first('categoryName') }}</strong>
+  </span>
+  @endif
+  <input type="submit" class='btn btn-primary btn-block' name="quiz" value="Submit and add Questions">
+</form>
+@endsection
