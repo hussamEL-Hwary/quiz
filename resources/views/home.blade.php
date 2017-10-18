b14deec9633b0966c23ca51b97139e8245a36e77
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2017-10-18 21:52:28 +0200

    view categories in home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
 
       @foreach($categories as $category)
       <div class='col-sm-6 col-md-4'>
-        <a class='link' href= '#'>
+        <a class='link' href= '/category/{{$category['id']}}'>
           <div class='box'>
             <h3 class="text-uppercase" >{{$category['name']}}</h3>
             <p>{{$category['description']}}<p>
