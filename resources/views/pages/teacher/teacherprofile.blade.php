abc115b835390ab041f575dcf72e9d25872ca17c
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2017-10-21 01:44:09 +0200

    fill teacher profile with data

--- resources/views/pages/teacher/teacherprofile.blade.php
+++ resources/views/pages/teacher/teacherprofile.blade.php
@@ -2,122 +2,44 @@
 
 @section('content')
 
-<div class="header">
-            <div class="container">
-                <div class="name">
-                    <h1>
-                    </h1>
-                    <p>{{ $user['job']}}</p>
-                </div>
-                <div class="links">
-                    <a href="https://facebook.com">
-                        <img src="/images/1.ico">
-                    </a>
-                    <a href="https://twitter.com">
-                        <img src="/images/2.ico">
-                    </a>
-                    <a href="https://google.com">
-                        <img src="/images/3.ico">
-                    </a>
-                    <a href="https://linkedin.com">
-                        <img src="/images/4.ico">
-                    </a>
-                </div>
-            </div>
+<section class='profile'>
+  <div class='container-fluid'>
+    <div class='row'>
+      <div class='col-md-3'>
+        <div class='information text-center'>
+          <img src="{{$user->profile->avatar}}" >
+          <ul class='info list-unstyled'>
+            <li> {{$user->first_name }}</li>
+            <li>{{$user->profile->job}}</li>
+            <li>{{$user->profile->education}}</li>
+            <li>{{$user->profile->bio}}</li>
+            <li></li>
+          </ul>
         </div>
+      </div>
 
-        <!-- Start Prifile -->
-
-        <div class="container">
-            <div class="profile">
-                <img src="{{$user->profile->avatar}}">
-                <div class="name1">
-                    <h3></h3>
-                </div>
-                <div class="about">
-                    <hr>
-                    <br>
-                    <div>
-                        <span>Birthday</span>
-                        <label></label>
-                    </div>
-                    <div>
-                        <span>Address</span>
-                        <label>{{ $user['address'] }}</label>
-                    </div>
-                    <div>
-                        <span>EMail</span>
-                        <label></label>
-                    </div>
-                    <div>
-                        <span>Phone</span>
-                        <label>{{$user->profile->job}}</label>
-                    </div>
-                    <div>
-                        <span>Website</span>
-                        <label><a href="https://www.facebook.com/ahmedelgendyfci">ِAhmed M. Elgendy</a></label>
-                    </div>
-                </div>
-                <div class="footer">
-
-                    <a class="ll" href="https://facebook.com">
-                        <img src="/images/1.ico">
-                    </a>
-                    <a href="https://twitter.com">
-                        <img src="/images/2.ico">
-                    </a>
-                    <a href="https://google.com">
-                        <img src="/images/3.ico">
-                    </a>
-
-                    <a href="https://linkedin.com">
-                        <img src="/images/4.ico">
-                    </a>
-
-                </div>
-                <button>Edite Your Profile</button>
+      <div class='col-md-9'>
+        <div class='content'>
+          <h2>Hussam's Quizess</h2>
+          @foreach($user->quiz as $quiz)
+          <div class='col-md-6'>
+            <a href='/quiz/{{$quiz->id}}'>
+            <div class='box'>
+              <ul class='list-unstyled'>
+                <li>{{$quiz->name}}</li>
+                <li>{{$quiz->category->name}}</li>
+                <li class='last'>{{$quiz->qcount}}</li>
+              </ul>
             </div>
+            </a>
+          </div>
+          @endforeach
+
         </div>
-
-        <!-- End Profile -->
-        <!-- Start Bio -->
-        <div class="container">
-            <div class="bio">
-                <h4>Bio</h4>
-                <p>{{ $user['bio'] }}</p>
-            </div>
-        </div>
-        <!-- End Bio -->
-        <!-- Start Education -->
-
-        <div class="container">
-            <div class="edu">
-                <h4>Education</h4>
-                <p>{{$user['education']}}</p>
-            </div>
-         </div>
-         <!-- End Education -->
-
-         <div class="container">
-            <div class="quiz">
-                <h4>Quizzes</h4>
-
-                <div class="first">
-                    <h3>
-                        <a href="#">C++ Algorithms</a>
-                    </h3>
-                    <p>I Studied At Faculty Of Computer And Information, Cairo University</p>
-                </div>
-                <div class="second">
-                        <h3>
-                            <a href="#">C++ Algorithms</a>
-                        </h3>
-                        <p>I Studied At Faculty Of Computer And Information, Cairo University</p>
-                </div>
-
-            </div>
-        </div>
-        <!---------------------------------------------->
+      </div>
+    </div>
+  </div>
+</section>
 
 
 @endsection
